e10bbdd2c9c2f52114e8face6e0f500ab9bc923d
Eugene <amubeer@gmail.com>
2023-01-17 15:23:20 +0300

    10: Add login/logout functionality
    After we've added login page and logout button (issue #11), we can go further and add login/logout functionality.
    Login/logout functionality is based on "sessions" mechanism. Yes, we could use already implemented solutions like [flask-login](https://github.com/maxcountryman/flask-login#flask-login), or even `session` object from the `flask` itself. But in that case we will not understand how login/logout works under the hood, because this library hides a lot of implementation details from the developer. So for learning purposes we will try to implement simple session-management system by hand.
    
    In the scope of this task we need to add an ability to login/logout for registered users.
    
    Steps to do:
    
        - add db table `user_session` which will keep our user's session data (let's make it have only `id`, `session_id` and `user_id` fields)
        - create a migration for this table
    
    Implement login router which does following:
        - check user's login/password pair for the presence in database
        - generate `session_id` for this user and add a row to the special table for session data we created above
        - return redirect response to the index page with generated session id attached using `Set-Cookie` header
    
    Implement logout router which does following:
        - get session-id from headers, remove according row from db, return redirect response to the login page
    
    Update templates:
        - add "Sign Out" button visible only for logged in users to the header of the base template
        - make "Sign In" and "Sign Up" buttons to be visible only for not logged in users

--- src/routes.py
+++ src/routes.py
@@ -1,11 +1,19 @@
 """forum123's route module."""
 
+from __future__ import annotations
+
+import uuid
+from typing import TYPE_CHECKING
+
 from flask import Blueprint
-from flask import render_template
+from flask import make_response, redirect, render_template, request, url_for
 
 from src.database import session
 from src.forms import LoginForm, RegistrationForm
-from src.models import User
+from src.models import User, UserSession
+
+if TYPE_CHECKING:
+    from werkzeug.wrappers.response import Response
 
 
 bp = Blueprint("routes", __name__)
@@ -16,7 +24,14 @@ bp = Blueprint("routes", __name__)
 def index() -> str:
     """Handle index page."""
     users = session.query(User).all()
-    return render_template("index.html", users=users)
+    session_id = request.cookies.get("session_id")
+    user_session = session.query(UserSession).filter_by(session_id=session_id).first()
+
+    current_user = None
+    if user_session is not None:
+        current_user = session.query(User).filter_by(id=user_session.user_id).one()
+
+    return render_template("index.html", users=users, current_user=current_user)
 
 
 @bp.route("/registration", methods=["POST", "GET"])
@@ -31,8 +46,29 @@ def registration() -> str:
     return render_template("registration.html", form=form)
 
 
-@bp.route("/login")
-def login() -> str:
+@bp.route("/login", methods=["POST", "GET"])
+def login() -> str | Response:
     """Handle user's login form."""
     form = LoginForm()
+    if form.validate_on_submit():
+        user = session.query(User).filter_by(username=form.username.data).first()
+        if user and user.check_password(form.password.data):
+            session_id = str(uuid.uuid4())
+            user_session = UserSession(session_id=session_id, user_id=user.id)
+            session.add(user_session)
+            session.commit()
+
+            response = make_response(redirect(url_for("routes.index")))
+            response.set_cookie("session_id", session_id)
+            return response
     return render_template("login.html", form=form)
+
+
+@bp.route("/logout")
+def logout() -> Response:
+    """Log out users."""
+    session_id = request.cookies.get("session_id")
+    if session_to_delete := session.query(UserSession).filter_by(session_id=session_id).first():
+        session.delete(session_to_delete)
+        session.commit()
+    return redirect(url_for("routes.login"))
