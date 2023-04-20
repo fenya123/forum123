5eb13a37aa037b745cb9c81f256c5cede06b2408
Eugene <amubeer@gmail.com>
2023-04-20 16:31:08 +0300

    116: Add REST endpoint to create new topic
    We need an endpoint which will accept a POST request with json body, validate the request and create a new topic in db. `author_id` field should be filled by user id from JWT token. We should return 400 if request isn't valid. We should return 401 if JWT token isn't provided. We should return 403 if user from JWT token was not found in database.
    
    **Request example**
    ```python
    POST /topics
    {
        "description": "Some description",  # required non-blank string field
        "title": "Some title",  # required non-blank string field
    }
    ```
    
    **Response example**
    ```python
    {
        "id": 42,
        "author_id": 18,
        "description": "Some description",
        "title": "Some title",
    }
    ```
    
    So in the scope of this task we need to create endpoint which will create a new topic and return info about it.
    
    ***
    
    PS more about typing we used for `@authorized_acces` can be read [here](https://stackoverflow.com/questions/69027497/mypy-errors-for-decorator-w-arguments)

--- src/api/topics/routes.py
+++ src/api/topics/routes.py
@@ -1,6 +1,8 @@
 """topics endpoint module."""
 
-from typing import Any
+from __future__ import annotations
+
+from typing import Any, TYPE_CHECKING
 
 from flask import abort
 from flask_restx import Namespace, reqparse, Resource
@@ -9,6 +11,8 @@ from src.api.auth.utils import authorized_access
 from src.api.topics.utils import parse_order_by
 from src.topics.models import Topic
 
+if TYPE_CHECKING:
+    from src.users.models import User
 
 ns = Namespace("topics", path="/topics")
 
@@ -17,10 +21,10 @@ parser = reqparse.RequestParser()
 
 @ns.route("")
 class TopicList(Resource):  # type: ignore
-    """Get a sorted list of all topics."""
+    """Work with topics."""
 
     @staticmethod
-    @authorized_access
+    @authorized_access()
     def get() -> list[dict[str, Any]] | None:
         """Get a sorted list of all topics."""
         parser.add_argument("order_by", type=parse_order_by, help="get a sorted list of topics")
@@ -34,13 +38,31 @@ class TopicList(Resource):  # type: ignore
             "title": topic.title,
         } for topic in topics]
 
+    @staticmethod
+    @authorized_access(provide_user=True)
+    def post(user: User) -> dict[str, Any]:
+        """Create topic and return its' info."""
+        parser.add_argument("title", required=True)
+        parser.add_argument("description", required=True)
+        topic_info = parser.parse_args()
+        if not topic_info["title"] or not topic_info["description"]:
+            abort(400, "invalid request")
+        created_topic = Topic.create_topic(title=topic_info["title"],
+                                           description=topic_info["description"], author_id=user.id)
+        return {
+            "id": created_topic.id,
+            "author_id": created_topic.author_id,
+            "description": created_topic.description,
+            "title": created_topic.title,
+        }
+
 
 @ns.route("/<int:topic_id>")
 class TopicInfo(Resource):  # type: ignore
     """Get a topic's information.."""
 
     @staticmethod
-    @authorized_access
+    @authorized_access()
     def get(topic_id: int) -> dict[str, Any] | None:
         """Get a topic's information."""
         if not (topic := Topic.get(topic_id)):
